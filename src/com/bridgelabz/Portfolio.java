97592ec8541ca4321277f6b6ebf92304aed9a2c4
josephxaviour <josephxaviour007@gmail.com>
2022-12-13 01:57:21 +0530

    [joseph] DAY11/12

---
+++ src/com/bridgelabz/Portfolio.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+public class PortFolio {
+    static ArrayList <Stock> array = new ArrayList<Stock>();
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        insertStockes();
+        displayStockes();
+        buyShares();
+    }
+
+    private static void insertStockes() {
+        System.out.println("How much stock you want to insert ..!");
+        int numberOfStock = sc.nextInt();
+        System.out.println("Insert stock name and stock prise  ");
+        for (int i = 1; i <= numberOfStock; i++) {
+            sc.nextLine();
+            String stockName = sc.nextLine();
+            int stockPrise = sc.nextInt();
+            array.add(new Stock(i, stockName, stockPrise));
+        }
+    }
+    private static void displayStockes(){
+
+        for(Stock stock : array){
+            System.out.println(stock.getNumberOfShares()+", "+stock.getStockName()+", "+stock.getSharePrize());
+        }
+    }
+    private static void buyShares(){
+        int totalValue = 0;
+        System.out.println("How much stock you want to buy ..!");
+        int numberOfStock = sc.nextInt();
+        String nameOfStock;
+        int quantity=0;
+        for (int i =1 ;i<= numberOfStock; i++){
+            System.out.println("Enter "+i+ " Share Name and Quantity of Share's ");
+            sc.nextLine();
+            nameOfStock = sc.nextLine();
+            quantity = sc.nextInt();
+            for (Stock arrayStock:array ){
+                if (arrayStock.getStockName().equals(nameOfStock)){
+                    totalValue += (quantity * arrayStock.getSharePrize());
+                }
+            }
+        }
+        System.out.println("Total values of shares is "+totalValue);
+    }
+
+}
